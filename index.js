8bd3328b75aee9ccbc64feb1b67e4c14fa3b97de
Cindy Arwinda Putri <cindyarwinda76@gmail.com>
2025-05-12 17:39:52 +0700

    concole log

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 
 // Menjalankan server
 app.listen(PORT, () => {
-  console.log(Server is running on http://localhost:${PORT});
+        console.log(`Server is running on http://localhost:${PORT}`);
 });
